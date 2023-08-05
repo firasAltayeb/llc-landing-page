4fecc4eb13ec972481c13a348dbd4f2115f4e23f
firas <tayebfiras@gmail.com>
2023-08-05 19:30:19 +0100

    Added project section and scroll functionality

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("scrollButton").addEventListener("click", function() {
+    document.getElementById("projects").scrollIntoView({ behavior: 'smooth' });
+  });
+  
